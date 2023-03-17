b1e133fff001eb4cf905a0c3e7ed20f63f383c72
yusuke.nakamura8 <yn1930221@gmail.com>
2023-03-17 20:49:47 +0900

    Index/NewServletの修正

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -31,13 +32,17 @@ public class IndexServlet extends HttpServlet {
         /**
          * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
          */
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            EntityManager em = DBUtil.createEntityManager();
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
 
-            List<Task> task = em.createNamedQuery("getAllTask", Task.class).getResultList();
-            response.getWriter().append(Integer.valueOf(task.size()).toString());
+        List<Task> task = em.createNamedQuery("getAllTask", Task.class).getResultList();
 
-            em.close();
-        }
+        em.close();
+
+        request.setAttribute("task", task);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/task/index.jsp");
+        rd.forward(request, response);
+    }
 
 }
--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,8 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
-import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
@@ -32,24 +30,12 @@ public class NewServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
-        em.getTransaction().begin();
-
-        Task t = new Task();
-
-        String content = "hello";
-        t.setContent(content);
-
-        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-        t.setCreated_at(currentTime);
-        t.setUpdated_at(currentTime);
-
-        em.persist(t);;
-        em.getTransaction().commit();
-
-        response.getWriter().append(Integer.valueOf(t.getId()).toString());
-
-        em.close();
+        request.setAttribute("_token", request.getSession().getId());
+        
+        request.setAttribute("task", new Task() );
+        
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/task/new.jsp");
+        rd.forward(request, response);
     }
 
 }
